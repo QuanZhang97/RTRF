bd8f5699134047d8cd47619f8b75394baca2175d
Quan Zhang <97296586+QuanZhang97@users.noreply.github.com>
2022-01-13 22:42:58 +0800

    Update test_realdata.m

--- test_realdata.m
+++ test_realdata.m
@@ -1,7 +1,28 @@
-% Dec. 27, 2021, Y.F. Chen, apply radon transform to vertical and radial
-% components of the array data. 
-% Test radon transfer as a preprocessing step before deconvolution.
-% Jan. 11, 2021, Modified by Quan Zhang, real data preprocessing test for Zhang et al. 2022
+%{
+  Real data test for Radon transform based receiver function preprocessing
+  
+  written by Quan Zhang et al., 2022
+  
+  Copyright: Zhejiang University
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  
+  Reference: 
+  
+%}
+
 clear; clc; close all;
 addpath ./data
 addpath ./codes
