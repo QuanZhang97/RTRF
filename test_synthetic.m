ece28923687b62314f3c4d982be2cd9242723f6a
Quan Zhang <97296586+QuanZhang97@users.noreply.github.com>
2022-01-13 22:43:27 +0800

    Update test_synthetic.m

--- test_synthetic.m
+++ test_synthetic.m
@@ -1,7 +1,27 @@
-% Oct. 4, 2020, Yunfeng Chen, migrate the synthetic RFs from FD
-% Oct. 6, 2020, migrate multiple shots
-% Oct. 9, 2020, least-squares migration with CG
-% Dec. 30, 2021, Quan Zhang, radon transform preprocessing
+%{
+  Synthetic test for Radon transform based receiver function preprocessing
+  
+  written by Quan Zhang et al., 2022
+  
+  Copyright: Zhejiang University
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  
+  Reference: 
+  
+%}
 
 clear; clc; close all;
 addpath ./data
